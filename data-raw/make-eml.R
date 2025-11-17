05a8638a2b01c0d659b0d9efa6255ea49e82d5dd
alvizek <avizek@flowwest.com>
2025-11-17 15:36:00 -0800

    adds personnel and publish data updates

--- data-raw/make-eml.R
+++ data-raw/make-eml.R
@@ -1,4 +1,13 @@
 # Script to run the EMLassemblyline workflow to make EML
+remotes::install_github("FlowWest/EMLaide") #https://github.com/FlowWest/EMLaide
+library(EMLaide) # used to reserve an EDI number
+
+# Prior to running this script, please ensure you have an EDI account
+# Contact support@edirepository.org to create an account
+# Please add your user_id and password to your Renviron
+# usethis::edit_r_environ()
+# EDI_USER_ID = "INSERT USERNAME"
+# EDI_PASSWORD = "INSERT PASSWORD"
 
 # PLEASE UPDATE! ----------------------------------------------------------
 # TODO
@@ -12,6 +21,13 @@ geography <- "INSERT" # This is a description of the geography. For instance, "F
 coordinates <- c("INSERT", "INSERT","INSERT", "INSERT") # These are the bounding
 # coordinates entered as: North bounding, East bounding, South bounding, West bounding
 maintenance <- "INSERT" # This is how often the package will be maintained: daily, weekly, monthly, annually, complete
+
+# TODO - Run the code below to reserve a edi number for NEW packages
+# edi_number <- reserve_edi_id(user_id = Sys.getenv("EDI_USER_ID"),
+#                              password = Sys.getenv("EDI_PASSWORD"))
+# TODO - For existing packages, please insert the edi number plus 0.1. As you will,
+# be updating this package the version number will increase by 1.
+edi_number <- "INSERT"
 
 # Finish creating metadata templates --------------------------------------
 # Use helper code to create remaining metadata
@@ -47,6 +63,10 @@ end_date <- max(data_file$date)
 data_csv <- read_csv(here("metadata_templates","attributes_csv_template", paste0(attributes_metadata_csv_file_name, ".csv")))
 # Name this file as paste0("attributes_", "INSERT data file name", ".txt")
 write.table(data_csv, file = here("metadata_templates", paste0("attributes_", data_file_name, ".txt")), sep = "\t", row.names = F, quote = F)
+
+# Personnel metadata - csv to txt
+personnel_csv <- read_csv(here("metadata_templates", "personnel_csv_template","personnel.csv"))
+write.table(personnel_csv, file = here("metadata_templates","personnel.txt"), sep = "\t", row.names = F, quote = F)
 
 # Create categorical variables template (required when attributes templates
 # contains variables with a "categorical" class)
@@ -94,4 +114,7 @@ make_eml(
   geographic.description = geography,
   geographic.coordinates = coordinates,
   maintenance.description = maintenance,
-  data.table = data_file_name)
+  data.table = data_file_name,
+  user.id = Sys.getenv("EDI_USER_ID"),
+  user.domain = "EDI",
+  package.id = edi_number)
--- publish-data.R
+++ publish-data.R
@@ -10,12 +10,28 @@
 remotes::install_github("FlowWest/EMLaide") #https://github.com/FlowWest/EMLaide
 library(EMLaide)
 
-path_eml_file <- here("data-raw", "eml", "edi.00.00.xml") # Created using make-eml.R
 
+# PLEASE UPDATE -----------------------------------------------------------
+# TODO Update with the correct file name for the EML file you want to upload
+path_eml_file <- here("data-raw", "eml", "edi.00.00.xml") # Created using make-eml.R
+# TODO For an existing package that you are updating, please insert the existing EDI number.
+existing_edi_id <- "INSERT"
+
+
+# Publish data ------------------------------------------------------------
 # If this is a new package, use "upload_edi_package". If it is an update,
 # use "update_edi_package"
 # Recommend publishing on staging first! Update to "production" when ready
+
+# FOR NEW PACKAGES
 upload_edi_package(user_id = Sys.getenv("EDI_USER_ID"),
                    password = Sys.getenv("EDI_PASSWORD"),
                    eml_file_path = path_eml_file,
                    environment = "staging")
+
+# FOR EXISTING PACKAGES
+# update_edi_package(user_id = Sys.getenv("EDI_USER_ID"),
+#                    password = Sys.getenv("EDI_PASSWORD"),
+#                    existing_package_identifier =
+#                    eml_file_path = path_eml_file,
+#                    environment = "staging")
